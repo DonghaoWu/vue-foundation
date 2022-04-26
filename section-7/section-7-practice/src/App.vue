bee29a828c8dda618147a720b2a39ac2e13e9a7c
Donghao Wu <donghao.wu.us@gmail.com>
2022-04-26 18:13:30 -0400

    0426 finish section-7 practice.

---
+++ section-7/section-7-practice/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="main">
+    <header>
+      <h2>Section-7-practice</h2>
+    </header>
+    <section class="card-container">
+      <active-user
+        class="card"
+        :username="enteredName"
+        :age="enteredAge"
+      ></active-user>
+      <user-data class="card" @handle-change="handleChange"></user-data>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enteredName: '',
+      enteredAge: '',
+    };
+  },
+  methods: {
+    handleChange({ username, age }) {
+      this.enteredName = username;
+      this.enteredAge = age;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Jost', sans-serif;
+}
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.main {
+  margin-top: 5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card-container {
+  margin-top: 3rem;
+  display: flex;
+}
+
+.card {
+  width: 400px;
+  height: 400px;
+  margin: 1rem;
+  border: 5px solid grey;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ section-7/section-7-practice/src/components/UserData.vue
@@ -0,0 +1,56 @@
+<template>
+  <section>
+    <form @submit.prevent="submitChange">
+      <div class="input-field">
+        <label class="input-label">name:</label>
+        <input type="text" placeholder="Your name" v-model="username" />
+      </div>
+      <div class="input-field">
+        <label class="input-label">age:</label>
+        <input type="number" placeholder="Your name" v-model="age" />
+      </div>
+      <button>Submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ['handle-change'],
+  data() {
+    return {
+      username: '',
+      age: '',
+    };
+  },
+  methods: {
+    submitChange() {
+      this.$emit('handle-change', { username: this.username, age: this.age });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-field {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0.5rem;
+}
+
+.input-label {
+  margin-bottom: 0.5rem;
+  width: 200px;
+  font-size: 1.5rem;
+}
+
+input {
+  font-size: 1.5rem;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+}
+</style>
