d00f4a65893019a00984e07dc5ebe8613df1f666
Donghao Wu <donghao.wu.us@gmail.com>
2022-05-30 17:09:45 -0400

    phase-1, add child component, pass parameters.

---
+++ section-11/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/LearningResource';
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Google website',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ section-11/note.md
@@ -0,0 +1,9 @@
+1. npm install
+
+2.
+
+- APP.VUE
+
+```HTML
+<templace></template>
+```
